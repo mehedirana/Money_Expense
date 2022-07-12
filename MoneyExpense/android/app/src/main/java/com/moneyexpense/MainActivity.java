a954822fed42877a3e469674f40887b07336769a
mehedirana <mdmehedirana@gmail.com>
2022-07-12 23:02:52 +0600

    Splash screen added

--- MoneyExpense/android/app/src/main/java/com/moneyexpense/MainActivity.java
+++ MoneyExpense/android/app/src/main/java/com/moneyexpense/MainActivity.java
@@ -1,6 +1,8 @@
 package com.moneyexpense;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
@@ -13,6 +15,13 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "MoneyExpense";
+  }
+
+  @Override
+
+  protected void onCreate(Bundle savedInstanceState){
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 
   /**
